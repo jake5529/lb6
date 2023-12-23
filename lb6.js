61dd825e967296dcd2d7fec9c803c41d1fa4b4b8
jake5529 <O_ivan_fomichev_O@mail.ru>
2023-12-23 17:22:34 +0700

    Add files via upload

---
+++ lb6.js
@@ -0,0 +1,16 @@
+function f(x) {
+    if (x % 2 === 0) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+  
+  console.log(f(4));
+  console.log(f(3));
+  console.log(f(-1));
+  console.log(f(0));
+  console.log(f(1));  
+
+
+
